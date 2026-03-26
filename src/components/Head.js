c4b05bce14a28636b1670cec58b8d16ad9d29d9c
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-03-26 11:13:07 +0530

    toggle functinility  of sidebar included using redux

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { FiSearch } from "react-icons/fi";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-          className="h-8"
+          onClick={() => toggleMenuHandler()}
+          className="h-8 cursor-pointer"
           alt="menu"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0evWy6vmua96UkF8RqHQv-SoBcuu3V9fwZw&s"
         />
-        <img
-          className="h-8 mx-2"
-          alt="youtube-logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png"
-        />
+        <a href="/">
+          <img
+            className="h-8 mx-2"
+            alt="youtube-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png"
+          />
+        </a>
       </div>
 
       <div className="col-span-10 pl-28">
