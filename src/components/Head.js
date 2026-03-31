6c3b0a92089713c773deed0b155eed141fdcacc4
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-03-31 10:07:33 +0530

    commit

--- src/components/Head.js
+++ src/components/Head.js
@@ -27,7 +27,6 @@ const Head = () => {
   }, [searchQuery]);
 
   const getSearchSuggestions = async () => {
-    console.log("API Call - " + searchQuery);
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
 
