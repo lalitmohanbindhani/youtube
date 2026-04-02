ddda0e8550e83d0c7aaa5668858f62b618ef6613
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-04-02 11:44:52 +0530

    live chat implemented

---
+++ src/components/LiveChat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LiveChat = () => {
+  return (
+    <div className="ml-2 p-2 border border-black w-full h-[450px]">
+      LiveChat
+    </div>
+  );
+};
+
+export default LiveChat;
