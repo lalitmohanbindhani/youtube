3cb87d4e82b72bfde4f228de3207158d63ba0dfb
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-04-02 19:52:07 +0530

    chatSlice implemented

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
+  useEffect(() => {
+    const i = setTimeout(() => {
+      //api polling
+      console.log("Api polling");
+    }, 2000);
+    return () => clearTimeout(i);
+  }, []);
   return (
-    <div className="ml-2 p-2 border border-black w-full h-[450px]">
-      LiveChat
+    <div className="ml-2 p-2 border border-black w-full h-[450px] bg-slate-100 rounded-lg">
+      <ChatMessage name="Lalit Bindhani" message="This is Namaste React" />
     </div>
   );
 };
