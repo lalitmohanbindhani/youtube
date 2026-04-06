f85c56b4e196aa95c2372cb92e12b730c19e822b
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-04-06 09:58:01 +0530

    useMemo concept

---
+++ src/components/Demo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Demo = () => {
+  return <div className="border border-black m-4 p-2 w-96 h-96">Demo</div>;
+};
+
+export default Demo;
--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,18 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomNames, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat.messages);
   useEffect(() => {
-    const i = setTimeout(() => {
+    const i = setInterval(() => {
       //api polling
-      console.log("Api polling");
-    }, 2000);
-    return () => clearTimeout(i);
+
+      dispatch(
+        addMessage({
+          name: generateRandomNames(),
+          message: makeRandomMessage(20) + " 🚀",
+        }),
+      );
+    }, 500);
+    return () => clearInterval(i);
   }, []);
   return (
-    <div className="ml-2 p-2 border border-black w-full h-[450px] bg-slate-100 rounded-lg">
-      <ChatMessage name="Lalit Bindhani" message="This is Namaste React" />
-    </div>
+    <>
+      <div className="ml-2 p-2 border border-black w-full h-[450px] bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {chatMessages.map((msg, i) => (
+            <ChatMessage key={i} name={msg.name} message={msg.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className="w-full p-2 ml-2 border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("On form Submit " + liveMessage);
+          dispatch(
+            addMessage({
+              name: "Lalit",
+              message: liveMessage,
+            }),
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="w-[360px]"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
+      </form>
+    </>
   );
 };
 
